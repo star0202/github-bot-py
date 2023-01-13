3e4f171937943a70364683ee10c11c6642c8ddc3
Starcea <stardev.uwu@gmail.com>
2023-01-13 13:57:42 +0900

    fix: typing

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from time import mktime
-from typing import Dict, List, Optional, Union
+from typing import Dict, List, Optional
 
 from discord import Embed, SelectOption
 from pytz import timezone
