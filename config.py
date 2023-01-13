e35ec6aef2020b8208cf225fef4318ec05becb6b
Starcea <stardev.uwu@gmail.com>
2023-01-13 15:24:06 +0900

    refactor: using final

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-TEST_GUILD_ID: list[int] = [1007937847451471884]
-STATUS: str = ""
-COLOR: int = 0x2f3136
-BAD: int = 0xed4245
+from typing import Final
+
+TEST_GUILD_ID: Final[list[int]] = [1007937847451471884]
+STATUS: Final[str] = ""
+COLOR: Final[int] = 0x2f3136
+BAD: Final[int] = 0xed4245
